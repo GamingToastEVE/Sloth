8cc621bc8731c41b96aec3ef9b1780171622a9be
GamingToastEVE <marontom@web.de>
2024-07-19 09:59:40 +0200

    Updated databaseHandler

--- src/main/java/org/ToastiCodingStuff/Delta/databaseHandler.java
+++ src/main/java/org/ToastiCodingStuff/Delta/databaseHandler.java
@@ -105,4 +105,105 @@ public class databaseHandler {
             throw new RuntimeException(e);
         }
     }
+
+    public int getMaxWarns (String guildID) {
+        try {
+            String getMaxWarnsString = "SELECT max_warns FROM warn_system_settings WHERE guild_id=?";
+            PreparedStatement getMaxWarnsStatement = connection.prepareStatement(getMaxWarnsString);
+            getMaxWarnsStatement.setString(1, guildID);
+            ResultSet rs = getMaxWarnsStatement.executeQuery();
+            if (rs == null) {
+                return 0;
+            }
+            return rs.getInt("max_warns");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getTimeMuted (String guildID) {
+        try {
+            String getMinutesMutedString = "SELECT minutes_muted FROM warn_system_settings WHERE guild_id=?";
+            PreparedStatement getMinutesMutedStatement = connection.prepareStatement(getMinutesMutedString);
+            getMinutesMutedStatement.setString(1, guildID);
+            ResultSet rs = getMinutesMutedStatement.executeQuery();
+            if (rs == null) {
+                return 0;
+            }
+            return rs.getInt("minutes_muted");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getWarnTimeHours (String guildID) {
+        try {
+            String getWarnTimeHoursString = "SELECT warn_time_hours FROM warn_system_settings WHERE guild_id=?";
+            PreparedStatement getWarnTimeHoursStatement = connection.prepareStatement(getWarnTimeHoursString);
+            getWarnTimeHoursStatement.setString(1, guildID);
+            ResultSet rs = getWarnTimeHoursStatement.executeQuery();
+            if (rs == null) {
+                return 0;
+            }
+            return rs.getInt("warn_time_hours");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getWarnRoleID (String guildID) {
+        try {
+            String getRoleIDString = "SELECT role_id FROM warn_system_settings WHERE guild_id=?";
+            PreparedStatement getRoleIDStatement = connection.prepareStatement(getRoleIDString);
+            getRoleIDStatement.setString(1, guildID);
+            ResultSet rs = getRoleIDStatement.executeQuery();
+            if (rs == null) {
+                return "0";
+            }
+            return rs.getString("role_id");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setWarnSettings (String guildID, int maxWarns, int minutesMuted, String roleID, int warnTimeHours) {
+        try {
+            if (hasWarnSystemSettings(guildID)) {
+                String setWarnSettings1 = "UPDATE warn_system_settings SET max_warns=?, minutes_muted=?, role_id=?, warn_time_hours=? WHERE guild_id=?";
+                PreparedStatement setWarnSettings2 = connection.prepareStatement(setWarnSettings1);
+                setWarnSettings2.setInt(1, maxWarns);
+                setWarnSettings2.setInt(2, minutesMuted);
+                setWarnSettings2.setString(3, roleID);
+                setWarnSettings2.setInt(4, warnTimeHours);
+                setWarnSettings2.execute();
+                return;
+            }
+            String setWarnSettings3 = "INSERT INTO warn_system_settings(guildid, max_warns, minutes_muted, role_id, warn_time_hours) VALUES(?,?,?,?)";
+            PreparedStatement setWarnSettings4 = connection.prepareStatement(setWarnSettings3);
+            setWarnSettings4.setInt(1, maxWarns);
+            setWarnSettings4.setInt(2, minutesMuted);
+            setWarnSettings4.setString(3, roleID);
+            setWarnSettings4.setInt(4, warnTimeHours);
+            setWarnSettings4.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean userInWarnTable (String guildID, String userID) {
+        try {
+            String checkIfUserIsInGuildTable = "SELECT user_id FROM warn? WHERE user_id=?";
+            PreparedStatement checkIfUserIsInGuildTableStatement = connection.prepareStatement(checkIfUserIsInGuildTable);
+            checkIfUserIsInGuildTableStatement.setString(1, guildID);
+            checkIfUserIsInGuildTableStatement.setString(2, userID);
+            ResultSet rs = checkIfUserIsInGuildTableStatement.executeQuery();
+            if (rs == null) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
