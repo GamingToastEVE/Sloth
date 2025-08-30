1497542e01083a2f5417fa1f83df9ddc8f2aadde
GamingToastEVE <marontom@web.de>
2025-08-30 18:18:23 +0200

    Updated databaseHandler

---
+++ src/main/java/org/ToastiCodingStuff/Delta/BanCommand.java
@@ -0,0 +1,33 @@
+package org.ToastiCodingStuff.Delta;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+public class BanCommand extends ListenerAdapter {
+
+    public BanCommand() {
+        // Constructor can be used for initialization if needed
+    }
+
+    @Override
+    public void onSlashCommandInteraction(net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent event) {
+        if (event.getName().equals("ban")) {
+            if (event.getMember().hasPermission(net.dv8tion.jda.api.Permission.BAN_MEMBERS)) {
+                if (event.getOption("user") != null) {
+                    net.dv8tion.jda.api.entities.Member member = event.getOption("user").getAsMember();
+                    if (member != null) {
+                        member.ban(0, TimeUnit.SECONDS).queue(); // 0 means no delete messages
+                        event.reply("Banned " + member.getEffectiveName()).queue();
+                    } else {
+                        event.reply("User not found.").setEphemeral(true).queue();
+                    }
+                } else {
+                    event.reply("Please specify a user to ban.").setEphemeral(true).queue();
+                }
+            } else {
+                event.reply("You do not have permission to use this command.").setEphemeral(true).queue();
+            }
+        }
+    }
+}
