70c626c2ba532c44d5d61832a2e0a862ff900599
GamingToastEVE <104887635+GamingToastEVE@users.noreply.github.com>
2025-08-31 18:06:41 +0200

    Revert "Add web dashboard with Discord Single Sign-On for server management"

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     java
 }
 
-application.mainClass = "org.ToastiCodingStuff.Delta.CombinedApplication" //
+application.mainClass = "org.ToastiCodingStuff.Delta.Delta" //
 group = "org.ToastiCodingStuff"
 version = "1.0"
 
@@ -17,13 +17,6 @@ dependencies {
     implementation("net.dv8tion:JDA:$jdaVersion")
     implementation("org.xerial:sqlite-jdbc:3.46.0.0")
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
-    
-    // Web framework dependencies
-    implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.1.5")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client:3.1.5")
-    implementation("org.springframework.boot:spring-boot-starter-security:3.1.5")
-    implementation("org.springframework.session:spring-session-core:3.1.3")
 }
 
 tasks.withType<JavaCompile> {
@@ -31,6 +24,6 @@ tasks.withType<JavaCompile> {
     options.isIncremental = true
 
     // Set this to the version of java you want to use,
-    // the minimum required for JDA is 1.8, Spring Boot 3.x requires 17
-    sourceCompatibility = "17"
+    // the minimum required for JDA is 1.8
+    sourceCompatibility = "11"
 }
