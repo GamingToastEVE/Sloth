5c394b3ff0ce01275450a72f20cd90e7a0b1c9e0
GamingToastEVE <104887635+GamingToastEVE@users.noreply.github.com>
2026-01-05 14:40:29 +0100

    Update shadow plugin version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     java
-    id ("com.github.johnrengelman.shadow") version "8.1.1"
+    id ("com.gradleup.shadow") version "9.3.0"
 }
 
 application.mainClass = "org.ToastiCodingStuff.Sloth.Sloth"
